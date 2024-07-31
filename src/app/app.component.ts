9b587b7d83ab5d16a1629de69607147437e9cb91
gokhul <gokhul.tg@galvanizeme.com>
2024-08-01 00:17:23 +0530

    made card component dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'Saw this awesome tree during my hike today.',
+      imageUrl: 'assets/tree.jpeg',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountain',
+      content: "Here's a picture of a snowy mountain",
+      imageUrl: 'assets/mountain.jpeg',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking',
+      content: 'I did some biking today',
+      imageUrl: 'assets/biking.jpeg',
+    },
+  ];
 }
